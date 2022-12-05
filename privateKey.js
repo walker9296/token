c1aef1b28d9995fdedc2ca4ebdab33970b9f1c0f
walker9296 <walkeroro@gmail.com>
2022-12-06 03:46:45 +0800

    first commit

---
+++ privateKey.js
@@ -0,0 +1,34 @@
+const { exit } = require('process')
+const { bsv } = require('scryptlib');
+
+// fill in private key on testnet in WIF here
+const key = 'cRWuzuVGbXfu2tu4CQQ6LHYBuUY3DYWUpQyqQndbzoMN8qeKCcHT'
+// const key2 = ''
+// const key3 = ''
+
+if (!key) {
+  genPrivKey()
+}
+
+function genPrivKey() {
+  const newPrivKey = new bsv.PrivateKey.fromRandom('testnet')
+  console.log(`Missing private key, generating a new one ...
+Private key generated: '${newPrivKey.toWIF()}'
+You can fund its address '${newPrivKey.toAddress()}' from sCrypt faucet https://scrypt.io/#faucet`)
+  exit(-1)
+}
+
+const privateKey = new bsv.PrivateKey.fromWIF(key)
+
+// const privateKey2 = new bsv.PrivateKey.fromWIF(key2)
+
+// const privateKey3 = new bsv.PrivateKey.fromWIF(key3)
+
+//console.log('' + privateKey.toAddress())
+
+module.exports = {
+  privateKey,
+  // privateKey2,
+  // privateKey3,
+  genPrivKey
+}
